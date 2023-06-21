8f1d66755ad4a2343cb3095905b0bf1368caa934
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-06-21 13:28:21 +0000

    add botmanager.py.
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

---
+++ botmanager.py
@@ -0,0 +1,32 @@
+import yaml
+import json
+from loguru import logger
+
+global_setting = yaml.safe_load(open('./yamls/General.yaml', 'r', encoding='UTF-8'))
+rand_sents = {
+    "zh-cn": json.load(open('./jsons/zh-cn/rand_sents.json', 'r', encoding='UTF-8')),
+    "zh-hk": json.load(open('./jsons/zh-hk/rand_sents_hk.json', 'r', encoding='UTF-8')),
+}
+voices = yaml.safe_load(open('./yamls/Voices.yaml', 'r', encoding='UTF-8'))
+def bot_config(name: str):
+    try:
+        return global_setting[name]
+    except KeyError:
+        logger.error(f'{name}:No such thing')
+        return None
+
+
+def sents_config(name: str, lang: str):
+    try:
+        return rand_sents[lang][name]
+    except KeyError:
+        logger.error(f'{name}:No such thing in {lang}')
+        return None
+
+
+def students_voices(name: str):
+    try:
+        return voices[name]
+    except KeyError:
+        logger.error(f'{name}:No such thing')
+        return None
