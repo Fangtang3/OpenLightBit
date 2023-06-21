430b2ac2acb6c617395f4da3640162c2b1df6b94
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-06-21 14:11:02 +0000

    update modules/zh-cn/ver-for-openmariya.py.
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh-cn/ver-for-openmariya.py
+++ modules/zh-cn/ver-for-openmariya.py
@@ -16,7 +16,7 @@ channel = Channel.current()
 channel.name("版本查询-om1")
 channel.description("查看机器人当前版本")
 channel.author("Emerald-AM9")
-txt = "当前装载版本为OpenLightBit(with OpenMariya 1.2.7 plugins compatibility)\n"
+txt = "当前装载版本为OpenLightBit 2.0(with Mariya Stable 1.2.7 plugins compatibility)\n"
 
 
 @channel.use(
