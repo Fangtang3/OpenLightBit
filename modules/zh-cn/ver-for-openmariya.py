79e807daa43f80b2f4e829ca8fad904964729b29
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-06-22 08:24:30 +0000

    update modules/zh-cn/ver-for-openmariya.py.
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh-cn/ver-for-openmariya.py
+++ modules/zh-cn/ver-for-openmariya.py
@@ -13,10 +13,10 @@ import langmanager
 
 r = botmanager.sents_config('rand_sents', lang='zh-cn')
 channel = Channel.current()
-channel.name("版本查询-om1")
+channel.name("版本查询")
 channel.description("查看机器人当前版本")
 channel.author("Emerald-AM9")
-txt = "当前装载版本为OpenLightBit 2.0(build 1)(with Mariya Stable 1.2.7 plugins compatibility)\n"
+txt = "当前装载版本为OpenLightBit 2.0(Beta)(build 2)(with Mariya Stable 1.2.7 plugins compatibility)\n"
 
 
 @channel.use(
