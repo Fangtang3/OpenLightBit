16222e09d0a157aba955606edadc04b878290a4a
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-11 00:56:24 +0000

    自动创建yamls/linux_knowledge.yaml
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- definer.py
+++ definer.py
@@ -7,7 +7,22 @@ import yaml
 # 但有个缺点就是无法实现热更新（你还不至于懒到重启一次程序都不愿意罢？）
 #url_config_yaml = yaml.safe_load(open('./yamls/urls.yaml', 'r', encoding='UTF-8'))
 eji_yaml = yaml.safe_load(open('./yamls/eji.yaml', 'r', encoding='utf-8'))
-linux_knowledge_yaml = yaml.safe_load(open('./yamls/linux_knowledge.yaml', 'r', encoding='utf-8'))
+try:
+    linux_knowledge_yaml = yaml.safe_load(open('./yamls/linux_knowledge.yaml', 'r', encoding='utf-8'))
+except FileNotFoundError:
+    safe_file_write('./yamls/linux_knowledge.yaml', """linux_knowledge : [
+    "usr不等于user哦，而是\"Unix Software Resource\"的缩写",
+    "/etc目录存放的主要是配置文件，如用户信息、服务的启动脚本、常用服务的配置文件等",
+    "/bin目录存放的主要是系统命令，如sudo，普通用户和root用户都可以执行",
+    "rm是一个神奇的命令，用于删除文件，文件夹，目录，甚至是/,-rf参数表示的是强制执行，如果想删除一个或者大多个文件（文件夹），可以使用:sudo rm -rf file1 file2 file3或者sudo rm -rf folder1 folder2 folder3等",
+    "不同的系统拥有不同的包管理，如deb系统的包管理为apt，dpkg RHEL系统为yum，dnf（openSUSE为zypper），Arch系统主要为pacman等",
+    "mkdir命令主要用于创建文件夹/目录，用法为mkdir <folder name>",
+    "tar命令的参数有很多，如-c,-x,-t,-z,-j,-v,-f,-p,-P,-N,-exclude FILE等"
+
+    ]""")
+    logger.success(
+        './yamls/linux_knowledge.yaml 未创建，程序已自动创建.')
+    linux_knowledge_yaml = yaml.safe_load(open('./yamls/linux_knowledge.yaml', 'r', encoding='utf-8'))
 #update_log_yaml = yaml.safe_load(open('./yamls/update_log.yaml', 'r', encoding='utf-8'))
 rand_sentence_yaml = yaml.safe_load(open('./yamls/rand_sentence.yaml', 'r', encoding='utf-8'))
 
