4f2403c0edb23362aa0689562cd14ae05e2aa89d
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-12 08:10:05 +0000

    包装
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh_cn/bot-version.py
+++ modules/zh_cn/bot-version.py
@@ -1,3 +1,18 @@
+#    OpenLightBit-KuoHuBit
+#    Copyright (C) 2023  Emerald-AM9
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import random
 
 from graia.ariadne.app import Ariadne
@@ -18,7 +33,7 @@ channel.author("Emerald-AM9")
 
 r = definer.rand_sentence('rand_sentence')
 # 版本号随机句子的参数
-txt = "当前装载版本:OpenLightBit 2.3.0-preview.b161(Xuanhua)(with Mariya Stable 1.2.7 compatibility)\n质量更新版本:2.3\n紧急更新补丁版本:0\n"
+txt = "当前装载版本:OpenLightBit 2.3.0-preview.b200(Xuanhua)(with Mariya Stable 1.2.7 compatibility)\n质量更新版本:2.3\n紧急更新补丁版本:0\n"
 
 
 @channel.use(
