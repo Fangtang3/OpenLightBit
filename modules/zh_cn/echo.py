eaf4f44f66ec50073f46dca8c1a552f1cfb4ebeb
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-12 08:08:11 +0000

    注释
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh_cn/echo.py
+++ modules/zh_cn/echo.py
@@ -1,3 +1,18 @@
+#    OpenLightBit-KuoHuBit
+#    Copyright (C) 2023  Emerald-AM9
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage, ActiveGroupMessage
 from graia.ariadne.event.mirai import GroupRecallEvent
