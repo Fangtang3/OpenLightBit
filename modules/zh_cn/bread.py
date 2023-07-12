35703781b74ebb51251482336a090396d5687b6f
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-12 07:38:54 +0000

    注释
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh_cn/bread.py
+++ modules/zh_cn/bread.py
@@ -1,6 +1,17 @@
-#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
-
-#  本项目遵守 AGPL-3.0 协议，项目地址：https://github.com/daizihan233/MiraiHanBot
+#    OpenLightBit-KuoHuBit
+#    Copyright (C) 2023  Emerald-AM9
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import math
 import random
@@ -9,10 +20,13 @@ import time
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message.element import At, Plain
-from graia.ariadne.message.parser.base import DetectPrefix, MatchContent
+from graia.ariadne.message.element import At
+from graia.ariadne.message.element import Plain
+from graia.ariadne.message.parser.base import DetectPrefix
+from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
-from graia.ariadne.util.saya import listen, decorate
+from graia.ariadne.util.saya import listen
+from graia.ariadne.util.saya import decorate
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from loguru import logger
