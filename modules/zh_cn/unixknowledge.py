0814e2e514fd9da6b0c73c322820ff039534c2cf
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-12 08:02:20 +0000

    注释
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- modules/zh_cn/unixknowledge.py
+++ modules/zh_cn/unixknowledge.py
@@ -1,3 +1,18 @@
+#    OpenLightBit-KuoHuBit
+#    Copyright (C) 2023  Emerald-AM9
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import random
 
 from graia.ariadne.app import Ariadne
@@ -16,7 +31,7 @@ channel.name("linux_knowledge")
 channel.description("*nix芝士")
 channel.author("Emerald-AM9")
 
-# 读取json
+# Read json
 su = definer.bot_global('su_id')
 linux_knowledge = definer.linux_knowledge('linux_knowledge')
 
