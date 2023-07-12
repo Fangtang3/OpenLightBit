ce61e35f4671fb874270da635a5775b0a5ca2e35
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-07-12 07:29:37 +0000

    优化代码风格
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- langmanager.py
+++ langmanager.py
@@ -1,8 +1,24 @@
+#    OpenLightBit-KuoHuBit
+#    Copyright (C) 2023  Emerald-AM9
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import At
-from graia.ariadne.message.parser.base import DetectPrefix, MatchContent
+from graia.ariadne.message.parser.base import DetectPrefix
+from graia.ariadne.message.parser.base import MatchContent
 from graia.ariadne.model import Group
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
