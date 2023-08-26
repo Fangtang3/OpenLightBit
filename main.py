661a66709628b2f816ab4dbfba071fd9a48a41db
Emerald-AM9 <emeraldam9@ms-rss.com>
2023-08-26 03:55:11 +0000

    debug...
    Signed-off-by: Emerald-AM9 <emeraldam9@ms-rss.com>

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from rich.progress import track
 import botfunc
 import cache_var
 
-print ("Starting OpenLightBit 31.0-Milestone-2(Milestone Update)...")
+print ("Starting OpenLightBit 31.0-Milestone-1(Milestone Update)...")
 
 saya = create(Saya)
 app = Ariadne(
